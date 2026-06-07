8c1f33429b1f1e90e48ad1cf126469532c6b6933
Ebuka <oparaiheomaebuka@gmail.com>
2026-06-08 00:26:01 +0100

    Added Auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Home from './Pages/Home.jsx';
 import Auth from './Pages/Auth.jsx';
 import Checkout from './Pages/Checkout.jsx';
 import Navbar from './components/Navbar.jsx';
+import AuthProvider from './context/AuthContext.jsx';
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
